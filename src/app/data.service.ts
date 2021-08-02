28fb1a6a3ea774067f6401bcf8dd7b131bd495f3
vaishakchanguli <vaishakch8@gmail.com>
2021-08-02 09:50:40 +0530

    1 file modified

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 
 @Injectable({ providedIn: 'root' })
 export class DataService {
+  private API_KEY = '9ooxBVMRUg8SzNPKWGhSzLkmp0WcBir7';
   constructor(private http: HttpClient) {}
 
   public getWeatherData(searchText) {
